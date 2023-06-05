9418f36c00cc77313a8834fa4f78400696cb607b
207r1a05p6 <93361352+207r1a05p6@users.noreply.github.com>
2023-06-05 21:40:51 +0530

    perform first DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Being Zero")
+var data1={image:"aab.jpeg",name:"Veldhandi Kavya",designation:"Student & developer"};
+var data2={image:"abcd.jpeg",name:"Kavya",designation:"Intern"};
+var isdata1=true;
+var displaydata;
+var changemypic=function(){
+     if(isdata1){
+        displaydata=data2;
+        isdata1=false;
+     }
+     else{
+        displaydata=data1;
+        isdata1=true;
+     }
+     doc.getElementById("myimage").src=displaydata.image;
+}
