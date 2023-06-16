8de1fc96e934afd6214d53ec8bc3cc9984d569cf
207r1a05p6 <93361352+207r1a05p6@users.noreply.github.com>
2023-06-16 19:37:12 +0530

    commit changes

--- weather.js
+++ weather.js
@@ -1,8 +1,11 @@
+
+// https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=eb7a2d4a5a9b284fe54b7bb547238443&units=metric
 const weatherApi={
     Key: "b5078df82bb2b670331f73c175bd13ae",
     BaseUrl: "https://api.openweathermap.org/data/2.5/weather?",
 }
 const searchInputBox=document.getElementById('input-box');
+
 searchInputBox.addEventListener('keypress',(event)=>{
     if(event.keyCode==13){
     console.log(searchInputBox.value);
@@ -12,17 +15,20 @@ searchInputBox.addEventListener('keypress',(event)=>{
 });
 
 function getWeatherReport(city){
-    fetch(`$(weatherApi.BaseUrl)?q =${city}&appid=${weatherApi.Key}&units=metric`)
+    fetch(`$(weatherApi.BaseUrl)?q =${city}&weatherid=${Key}&units=metric`)
     .then(weather => {
         return weather.json();
     }).then(showweatherReport);
 }
 function showweatherReport(weather){
     console.log(weather);
+
     let city=document.getElementById('city');
     city.innerText=`${weather.name},${weather.sys.country}`;
+
     let temperature=document.getElementById('temp');
     temperature.innerHTML=`${Math.round(weather.main.temp)}&deg;C`;
+    
     let minMaxTemp=document.getElementById('min-max');
     minMaxTemp.innerHTML=`${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}`;
 
